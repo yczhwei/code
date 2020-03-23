41d533a1a7e3599ac3f275b97cfecbdaf3ead747
yczhwei <60326366+yczhwei@users.noreply.github.com>
2020-03-23 20:45:49 +0800

    Update app3.js

--- app3.js
+++ app3.js
@@ -10,13 +10,16 @@ function file_video(path){
       <source src="${url}" type="video/mp4">
     </video>
     <br>
-    <!-- 固定标签 -->
+    <!-- 固定标签
     <div class="mdui-textfield">
       <label class="mdui-textfield-label">您下载地址</label>
       <input class="mdui-textfield-input" type="text" value="${url}"/>
     </div>
+     -->
 </div>
+<!--
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+-->
     `;
     $('#content').html(content);
 };
